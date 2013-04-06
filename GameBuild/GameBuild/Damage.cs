b8fd69454edb59fc8cf1aacf746a4feaecd36efd
Mntndew <trickepiclyawesome@gmail.com>
2013-04-06 21:08:24 +0200

    testing gource

---
+++ GameBuild/GameBuild/Damage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameBuild
+{
+    public class Damage
+    {
+        public int damage;
+
+        Random rand = new Random();
+
+        public Damage()
+        {
+        }
+
+        public int dealDamage(int min, int max)
+        {
+            this.damage = rand.Next(min, max);
+            return damage;
+        }
+    }
+
+    public class DamageEffect
+    {
+        public Vector2 position;
+        string damageString;
+        Color color;
+        public bool active;
+
+        public DamageEffect(int damage, Game1 game, Vector2 position, Color color)
+        {
+            this.position = position;
+            this.color = color;
+            damageString = "" + damage;
+        }
+
+        public void Effect()
+        {
+            position.Y -= 1;
+            if (color.A > 5)
+            {
+                if (color.R > color.B)
+                {
+                    color.A -= 7;
+                    color.R -= 7;
+                    color.G -= 7;
+                    color.B -= 1;
+                }
+                else
+                {
+                    color.A -= 7;
+                    color.R -= 7;
+                    color.G -= 7;
+                    color.B -= 7;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Game1 game)
+        {
+            spriteBatch.DrawString(game.spriteFont, damageString, position, color);
+        }
+    }
+}
---
+++ GameBuild/GameBuild/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameBuild
+{
+    class Node
+    {
+        public int F;
+        public int G;
+        public int H;
+
+        public Node()
+        {
+        }
+    }
+}
